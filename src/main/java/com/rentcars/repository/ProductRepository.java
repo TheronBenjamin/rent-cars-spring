788d462d0df276e15b1651ec7139ee1b7853af36
Benjamin <benjamin.theron.bt@gmail.com>
2022-08-17 17:22:46 +0100

    Added ProductRepository.java

--- src/main/java/com/rentcars/repository/ProductRepository.java
+++ src/main/java/com/rentcars/repository/ProductRepository.java
@@ -2,6 +2,13 @@ package com.rentcars.repository;
 
 import com.rentcars.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
+
+    @Query(value = "SELECT MAX(id) FROM Product")
+    public Integer getMaxId();
+
 }
