57e0e4857f2ff894205ab97ab14421373a41ee72
Benjamin <benjamin.theron.bt@gmail.com>
2022-08-23 17:29:06 +0100

    Added mapping to ClientMapper + nullValuePropertyStrategy on userMapper

--- src/main/java/com/rentcars/mapper/ClientMapper.java
+++ src/main/java/com/rentcars/mapper/ClientMapper.java
@@ -3,13 +3,17 @@ package com.rentcars.mapper;
 import com.rentcars.api.dto.ClientDto;
 import com.rentcars.model.Client;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy =  NullValuePropertyMappingStrategy.IGNORE)
 public interface ClientMapper {
 
+    @Mapping(source = "clientDto.id" , target = "id")
     Client mapDtoToClient(ClientDto clientDto);
+    @Mapping(source = "id" , target = "id")
     ClientDto mapClientToDto(Client client);
 
 }
--- src/main/java/com/rentcars/mapper/UserMapper.java
+++ src/main/java/com/rentcars/mapper/UserMapper.java
@@ -3,10 +3,11 @@ package com.rentcars.mapper;
 import com.rentcars.api.dto.UserDto;
 import com.rentcars.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.NullValuePropertyMappingStrategy;
 import org.springframework.stereotype.Component;
 
 @Component
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring", nullValuePropertyMappingStrategy =  NullValuePropertyMappingStrategy.IGNORE)
 public interface UserMapper {
 
     User mapDtoToUser(UserDto userDto);
